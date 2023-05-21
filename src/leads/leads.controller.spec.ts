e3f372aa78c68946c37b23aa76101565b61ef87b
Caio Moura <caiomoura1994@gmail.com>
2023-05-21 20:31:48 -0300

    Create leads.controller tests

--- src/leads/leads.controller.spec.ts
+++ src/leads/leads.controller.spec.ts
@@ -1,43 +1,100 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaService } from '../prisma/prisma.service';
+import { Test } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+import { Prisma } from '@prisma/client';
+
 import { LeadsController } from './leads.controller';
 import { LeadsService } from './leads.service';
+import { FindAllLeadsDto } from './dto/findall-lead.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { generateMockLead } from '../utils/test-utils';
 
 describe('LeadsController', () => {
-  let controller: LeadsController;
-  let leadService: LeadsService;
-  let prismaService: PrismaService;
+  let leadsController: LeadsController;
+  let leadsService: LeadsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [LeadsController],
       providers: [LeadsService, PrismaService],
-    })
-      .compile();
+    }).compile();
 
-    controller = module.get<LeadsController>(LeadsController);
-
-    const mockPrismaService = {
-      lead: {
-        findMany: jest.fn().mockResolvedValue([]),
-        findUnique: jest.fn().mockResolvedValue({}),
-        update: jest.fn().mockResolvedValue({}),
-        create: jest.fn().mockResolvedValue({}),
-        delete: jest.fn().mockResolvedValue({}),
-      }
-    } as unknown as PrismaService
-    prismaService = mockPrismaService
-    leadService = new LeadsService(prismaService);
-    controller = new LeadsController(leadService);
-
+    leadsController = moduleRef.get<LeadsController>(LeadsController);
+    leadsService = moduleRef.get<LeadsService>(LeadsService);
   });
 
-  it('should be defined', async () => {
-    expect(controller).toBeDefined();
+  afterEach(async () => {
+    jest.clearAllMocks();
   });
 
-  it('FindAll ', async () => {
-    const allLeads = await controller.findAll({})
-    expect(allLeads).toHaveLength(0)
+  describe('create', () => {
+    it('should create a new lead', async () => {
+      const createLeadDto: Prisma.LeadCreateInput = generateMockLead(false);
+      const mockLead = {
+        ...createLeadDto,
+        id: faker.number.int(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.recent(),
+      };
+
+      jest.spyOn(leadsService, 'create').mockResolvedValue(mockLead);
+
+      const result = await leadsController.create(createLeadDto);
+
+      expect(leadsService.create).toHaveBeenCalledWith(createLeadDto);
+      expect(result).toEqual(mockLead);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of leads', async () => {
+      const params: FindAllLeadsDto = {}; // You can customize the params object if needed
+      const mockLeads = [
+        generateMockLead(true),
+        generateMockLead(true),
+      ];
+
+      jest.spyOn(leadsService, 'findAll').mockResolvedValue(mockLeads);
+
+      const result = await leadsController.findAll(params);
+
+      expect(leadsService.findAll).toHaveBeenCalledWith(params);
+      expect(result).toEqual(mockLeads);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a lead by ID', async () => {
+      const mockLead = generateMockLead(true);
+      jest.spyOn(leadsService, 'findOne').mockResolvedValue(mockLead);
+
+      const result = await leadsController.findOne(mockLead.id);
+
+      expect(leadsService.findOne).toHaveBeenCalledWith(mockLead.id);
+      expect(result).toEqual(mockLead);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a lead by ID', async () => {
+      const updateLeadDto: Prisma.LeadUpdateInput = generateMockLead(false);
+      const mockLead = generateMockLead(true);
+      jest.spyOn(leadsService, 'update').mockResolvedValue(mockLead);
+
+      const result = await leadsController.update(mockLead.id, updateLeadDto);
+      expect(leadsService.update).toHaveBeenCalledWith(mockLead.id, updateLeadDto);
+      expect(result).toEqual(mockLead);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a lead by ID', async () => {
+      const mockLead = generateMockLead(true);
+      jest.spyOn(leadsService, 'remove').mockResolvedValue(mockLead);
+
+      const result = await leadsController.remove(mockLead.id);
+
+      expect(leadsService.remove).toHaveBeenCalledWith(mockLead.id);
+      expect(result).toEqual(mockLead);
+    });
   });
 });
